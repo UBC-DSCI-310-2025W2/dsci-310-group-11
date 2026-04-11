bcfdb66e81c45020c8a63eea234820afcb00a6ed
Rudy Ma <legorudy@gmail.com>
2026-04-11 02:31:11 -0700

    added missing changes

--- scripts/boxplot_generator.py
+++ scripts/boxplot_generator.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 import click
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from src.plot_utils import create_quality_boxplot
+from winequalityutils import create_quality_boxplot
 
 
 @click.command()
